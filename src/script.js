658fa1d6cb68649a4dd27a68fd1b4aa30475c373
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-10-07 11:12:59 -0400

    tweaking css selectors

--- src/script.js
+++ src/script.js
@@ -7,19 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const { offsetWidth: width, offsetHeight: height } = eyes;
     let {offsetX: x, offsetY: y } = event;
     const walk = 110;
+
     if (this !== event.target) {
       x = x + event.target.offsetLeft;
       y = y + event.target.offsetTop;
     }
 
-
     const xWalk = Math.round((x/width * walk) - (walk/2));
     const yWalk = Math.round((y/height * walk) - (walk/2));
 
-    console.log(width);
-    console.log(height);
+    console.log(event.target.offsetLeft);
+    console.log(event.target.offsetTop);
     pupils.forEach(pupil => pupil.style.transform = `translate(${xWalk}px, ${yWalk}px)`);
   }
 
-  document.addEventListener('mousemove', followCursor);
+  eyes.addEventListener('mousemove', followCursor);
 });
